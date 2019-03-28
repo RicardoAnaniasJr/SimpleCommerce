7d27bb0ea14dd138f8dc95c78370f77ef53086d4
Ricardo <ricardoananiasjr@icloud.com>
2019-03-28 17:30:54 -0300

    Implementation of product classes

---
+++ SimpleCommerce/AbsProdutos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCommerce
+{
+    class AbsProdutos : IProdutos
+    {
+        public virtual double Preco { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public virtual string Fornecedor { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public virtual int Quantidade { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ SimpleCommerce/IProdutos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCommerce
+{
+    interface IProdutos
+    {
+        double Preco { get; set; }
+        string Fornecedor { get; set; }
+        int Quantidade { get; set; }
+
+    }
+}
